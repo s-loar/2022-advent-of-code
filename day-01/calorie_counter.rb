5de13549bf0c15febd682e9f932554052425f0bd
s-loar <sjl7909@gmail.com>
2022-12-07 13:21:49 -0500

    2nd problem for day 01

--- day-01/calorie_counter.rb
+++ day-01/calorie_counter.rb
@@ -15,6 +15,7 @@ class CalorieCounter
   def find_max_carried
     carried_calories = 0
     @max_calorie = 0
+
     @calories.each do |calorie|
       if calorie == 0
         @max_calorie = carried_calories if carried_calories > @max_calorie
@@ -23,7 +24,23 @@ class CalorieCounter
         carried_calories += calorie
       end
     end
-    puts "Max carried = #{@max_calorie}"
+    @max_calorie
+  end
+
+  def sum_of_top_3
+    # put all sums in an array, sort by total and pick the top 3 then sum and return
+    carried_calories = 0
+    calorie_totals = []
+
+    @calories.each do |calorie|
+      if calorie == 0
+        calorie_totals << carried_calories
+        carried_calories = 0
+      else
+        carried_calories += calorie
+      end
+    end
+    calorie_totals.sort.reverse.take(3).sum
   end
 
   private
--- day-01/solution.rb
+++ day-01/solution.rb
@@ -4,5 +4,6 @@ calorie_counter = CalorieCounter.new
 
 # calorie_counter.write_rows
 
-puts "1st problem:"
-calorie_counter.find_max_carried
+puts "1st problem: #{calorie_counter.find_max_carried}"
+
+puts "2nd problem: #{calorie_counter.sum_of_top_3}"
